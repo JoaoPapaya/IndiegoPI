38323981f97a1cfdcd8537ff316f181864f183e7
João Barboza <jbarbozamendonca@gmail.com>
2025-08-15 15:05:16 -0300

    ajustes estéticos

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -15,7 +15,6 @@ function Projetos() {
                     Projeto 3
                 </a>
             </div>
-
             <h2 className={styles.h2}>Projetos Sugeridos</h2>
             <div className={styles.sugeridosSecao}>
                 <a className={styles['icone-projeto']} href="#">
