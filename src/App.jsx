2a7c9e1d3e1a042e68d83a3f4195c6ab70675a8f
João Barboza <jbarbozamendonca@gmail.com>
2025-08-18 15:15:03 -0300

    adicao dos precos dos projetos

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <div>
       <BarraDePesquisa />
       <BotaoDePerfil />
-      <ProjetosSugeridos nome1="Sugestão 1" nome2="Sugestão 2" nome3="Sugestão 3" />
-      <ProjetosNovos nome1="Projeto novo 1" nome2="Projeto novo 2" nome3="Projeto novo 3" />
+      <ProjetosSugeridos nome1="Sugestão 1" nome2="Sugestão 2" nome3="Sugestão 3" preco1={20.00} preco2={35.90} preco3={0.00} />
+      <ProjetosNovos nome1="Projeto novo 1" nome2="Projeto novo 2" nome3="Projeto novo 3" preco1={12.00} preco2={69.99} preco3={80.50} />
       <Navegação />
     </div>
   );
--- src/components/ProjetosSugeridos.jsx
+++ src/components/ProjetosSugeridos.jsx
@@ -1,18 +1,24 @@
 import styles from './Projetos.module.css';
 
-function ProjetosSugeridos({ nome1, nome2, nome3 }) {
+function ProjetosSugeridos({ nome1, nome2, nome3, preco1, preco2, preco3 }) {
     return (
         <div>
             <h2 className={styles.h2}>Projetos Sugeridos</h2>
             <div className={styles.sugeridosSecao}>
                 <a className={styles['icone-projeto']} href="#">
                     {nome1}
+                    <br />
+                    R${preco1}
                 </a>
                 <a className={styles['icone-projeto']} href="#">
                     {nome2}
+                    <br />
+                    R${preco2}
                 </a>
                 <a className={styles['icone-projeto']} href="#">
                     {nome3}
+                    <br />
+                    R${preco3}
                 </a>
             </div>
         </div>
