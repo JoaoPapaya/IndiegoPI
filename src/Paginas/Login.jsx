83f71dcbc4970c64584fd0b6681fde3819eed673
Rodrigo Vilemen <rodrigo.vilemen@hotmail.com>
2025-08-22 16:09:58 -0300

    apresentação

--- src/Paginas/Login.jsx
+++ src/Paginas/Login.jsx
@@ -1,10 +1,13 @@
 import EmailInput from "../components/EmailInput"
 import LoginButton from "../components/LoginButton"
 import PasswordInput from "../components/PasswordInput";
-function Login() { 
+import styles from '../components/LoginButton.module.css'
+import styles2 from '../components/BarraDePesquisa.module.css'
+
+function Login() {
    return (
-     <div>
-      <EmailInput />
+     <div className={styles.container}>
+      <EmailInput className={styles2.inputCustom}/>
       <PasswordInput />
       <LoginButton />
      </div>
