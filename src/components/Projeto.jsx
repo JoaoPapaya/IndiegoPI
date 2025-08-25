aaf2e26c0805133650998fed04430594fa706ed6
João Barboza <jbarbozamendonca@gmail.com>
2025-08-25 14:36:45 -0300

    branch

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -3,6 +3,7 @@ import styles from './Projetos.module.css';
 function Projeto({ nome, preco }) {
     return (
         <div>
+            
                 <p className={styles['icone-projeto']} href="#">
                     <h2>{nome}</h2>
                     <br />
