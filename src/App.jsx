10ae3494246d47e7302ad35d0023dbc69705edb4
Fernando Soares <fernandossoares9@gmail.com>
2025-08-25 15:09:05 -0300

    ultimo desafio

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Paginas/Home'; 
 import Login from './Paginas/Login';
 import Cadastro from './Paginas/Cadastro';
+import Projeto from './Paginas/Projeto'
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Início</Link> | <Link to="/login">Login</Link> | <Link to="/cadastro">Cadastro</Link>
+        <Link to="/">Início</Link> | <Link to="/login">Login</Link> | <Link to="/cadastro">Cadastro</Link> | <Link to="Projeto">Projeto</Link> 
       </nav>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/projeto" element={<Projeto />} />
       </Routes>
     </div>
   );
