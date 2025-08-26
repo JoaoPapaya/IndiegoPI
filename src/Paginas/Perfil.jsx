f4e29c5eac621dc28825cffe2860f0b01b45ed67
João Barboza <jbarbozamendonca@gmail.com>
2025-08-26 14:27:13 -0300

    tela de perfil final

--- src/Paginas/Perfil.jsx
+++ src/Paginas/Perfil.jsx
@@ -1,5 +1,5 @@
 import styles from "../components/BotaoDePerfil.module.css";
-import styles2 from "../components/ButtonCustom.module.css";
+import styles2 from "../components/ButtonPerfil.module.css";
 
 function Perfil(){
     
@@ -9,13 +9,13 @@ function Perfil(){
             <img src="https://i.pinimg.com/736x/ce/40/50/ce4050f967af48a2f0700e1304d2d513.jpg" width={300} className={styles.FotoPaginaPerfil}/>
         </div>
         <div className={styles.containerCentro}>
-            <button className={styles2.ButtonCustom}>teste</button>
+            <button className={styles2.ButtonCustom}>Suporte</button>
         </div>
         <div className={styles.containerCentro}>
-            <button className={styles2.ButtonCustom}>teste</button>
+            <button className={styles2.ButtonCustom}>Configurações</button>
         </div>
         <div className={styles.containerCentro}>
-            <button className={styles2.ButtonCustom}>teste</button>
+            <button className={styles2.ButtonCustom}>Amigos</button>
         </div>                
     </>
 )
